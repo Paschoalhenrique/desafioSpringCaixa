b01053b43f1b3c93b97ae4606613e041df91fa65
paschoalhenrique <paschoalhenrique@hotmail.com>
2025-02-26 23:16:54 -0300

    Criação da classe model para mapeamento com nome diferente de produto

---
+++ src/main/java/com/desafio/produtosapi/model/Produto.java
@@ -0,0 +1,66 @@
+package com.desafio.produtosapi.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//POJO -> java Object
+@Entity
+@Table (name = "produto")
+public class Produto {
+
+    @Id
+    @Column(name = "id")// caso for necessarios fazer mapeamento com nome da coluna com nome diferente
+    private String id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String descricao;
+
+    @Column
+    private Double preco;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome= '" + nome + '\'' +
+                ", descricao= '" + descricao + '\'' +
+                ", preço= " + preco +
+                '}';
+    }
+}
